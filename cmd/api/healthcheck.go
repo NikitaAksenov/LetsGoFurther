f01da8e906358bc9c64132dcdb2e10252736e424
NikitaAksenov <naksenov96@gmail.com>
2025-11-09 11:45:55 +0300

    Chapter 3.6. Sending Error Messages

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -15,8 +15,6 @@ func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The error encountered a problem and cannot process your request", http.StatusInternalServerError)
-		return
+		app.serverErrorResponse(w, r, err)
 	}
 }
