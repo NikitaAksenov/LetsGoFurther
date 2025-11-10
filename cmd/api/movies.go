cf5d3c83a4765033059a90ca0810889f8e65c423
NikitaAksenov <naksenov96@gmail.com>
2025-11-10 19:11:19 +0300

    Chapter 4.4. Custom JSON Decoding

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -12,10 +12,10 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	fmt.Fprintln(w, "create a new movie")
 
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year,omitzero"`
-		Runtime int32    `json:"runtime,omitzero"`
-		Genres  []string `json:"genres,omitzero"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
