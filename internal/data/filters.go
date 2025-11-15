81b2c053c090adc78cfddcb55ff21e030780836a
NikitaAksenov <naksenov96@gmail.com>
2025-11-15 13:59:24 +0300

    Chapter 9.2. Validating Query String Parameters

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,7 +1,20 @@
 package data
 
+import "github.com/NikitaAksenov/LetsGoFurther/internal/validator"
+
 type Filters struct {
-	Page     int
-	PageSize int
-	Sort     string
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafelist []string
+}
+
+func ValidateFilters(v *validator.Validator, f Filters) {
+	v.Check(f.Page > 0, "page", "must be greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "must not be greater than 10 million")
+
+	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "must not be greater than 100")
+
+	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
